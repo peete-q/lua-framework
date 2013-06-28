b5db8fd9cf421b41f15be3caf19932afc18c6378
peete-q <peter_qy@sina.cn>
2013-06-28 17:56:01 +0800

    Create test_class.lua

---
+++ test_class.lua
@@ -0,0 +1,41 @@
+
+dofile 'test2.lua'
+
+classA = class 'classA' define {
+  a = 'classA.a',
+	b = {
+		c = 'classA.b.c'
+	},
+	f = function(self, ...)
+		print('classA', ...)
+	end,
+}
+a = classA()
+
+classB = class 'classB' inherit 'classA' define {
+	f = function(self, ...)
+		self.__base:f(...)
+		print('classB', ...)
+	end,
+}
+b = classB()
+
+classC = class 'classC' inherit 'classB' define {
+	f = function(self, ...)
+		self.__base:f(...)
+		print('classC', ...)
+	end,
+}
+c = classC()
+
+print '-----------'
+a:f('a')
+print '-----------'
+b:f('b')
+print '-----------'
+b.__base:f('b')
+print '-----------'
+c:f('c')
+print '-----------'
+c.__base:f('c')
+c.__base.__base:f('c')
