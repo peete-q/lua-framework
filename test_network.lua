5ce79c626fd2b185706611c6e9418b7d6fab9699
peete-q <peter_qy@sina.cn>
2013-07-03 15:23:58 +0800

    Update test_network.lua

--- test_network.lua
+++ test_network.lua
@@ -1,37 +1,34 @@
-dofile 'network.lua'
+dofile "network.lua"
 
 -- server
 local cmd = {
 	hello = function(...)
-		print('hello', ...)
+		print("hello", ...)
 	end,
 	hi = function(...)
-		print('hi', ...)
-		return 'hi_ack'
+		print("hi", ...)
+		return "hi_ack"
 	end,
-	sub = {a = print, b=print},
+	sub = {a = print, b = print},
 }
-network.listen('127.0.0.1',10001, function(c)
-	print('accept', c) 
-	con = c
-	con:setReceivable(true)
-	con:addPrivilege('cmd',cmd)
-	con:setReceiver(function(s) print('receive', s) end)
+network.listen("127.0.0.1",10001, function(c)
+	print("accept", c) 
+	c:addPrivilege("cmd",cmd)
+	c:setReceiver(function(s) print("receive", s) end)
 end)
 
 -- client
-network.connect('127.0.0.1',10001, function(c, e)
+network.connect("127.0.0.1",10001, function(c, e)
 	if not c then
-		print('connect failed', e)
+		print("connect failed", e)
 		return
 	end
-	c:setReceivable(true)
 	network.step(1)
 	local h = c.cmd.hi(1,2,3)
-	h.onAck = function(...) print('ack', ...) end
+	h.onAck = function(...) print("ack", ...) end
 	c.cmd.hello(1,2,3)
-	c.cmd.sub.a('sub.a')
-	c:send'xxxxxxx'
+	c.cmd.sub.a("sub.a")
+	c:send"xxxxxxx"
 	for i = 1, 10 do
 		network.step(1)
 	end
